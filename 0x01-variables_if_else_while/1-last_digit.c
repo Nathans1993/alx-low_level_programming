42a5190c82006bf84b209b297a2b0a484f7bfc7b
Nathaniel Joshua <natha113322@gmail.com>
2021-06-18 04:27:47 -0700

    Greater  number and Smaller number using Random generation

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* main - Determine either greater than 5, is less than 6, or 0 */
+
+
+/* Return: Always 0 (success) */
+int main(void)
+{
+	int n, i;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i = n % 10;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and less than 6 and not 0\n", n, i);
+	return (0);

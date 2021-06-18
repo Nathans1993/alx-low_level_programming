8620f994adf06177e7611a66fff21814e5536a42
Nathaniel Joshua <natha113322@gmail.com>
2021-06-18 05:20:51 -0700

    Greater than and less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,32 +2,29 @@
 #include <time.h>
 #include <stdio.h>
 
-
 /**
- * main - Determine either greater than 5, is less than 6, or 0 
+ * main - Determines either greater than 5, is less than 6, or is 0
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, i;
-
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	i = n % 10;
-	if (i > 5)
+	l = n % 10;
+	if (l > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (i == 0)
+	else if (l == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, i);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, i);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
-

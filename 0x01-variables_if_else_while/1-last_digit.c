bf6c8cfc53366ec42d776aef1851fdf7e9ee03a7
Nathaniel Joshua <natha113322@gmail.com>
2021-06-18 04:33:02 -0700

    Greater  number and Smaller number using Random generation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	i = n % 10;
-	if (n > 5)
+	if (i > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, i);
 	}

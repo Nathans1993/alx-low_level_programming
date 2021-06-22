f35538c0ec3a21719e046f9fb127afed5377f7ab
Nathaniel Joshua <natha113322@gmail.com>
2021-06-22 07:17:07 -0700

    functions

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,23 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+/**
+ * Functions Prototypes
+ * 
+ * Stating from _putchar to end
+ */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif

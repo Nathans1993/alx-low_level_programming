0fb5516ae6bdbd83b2ebe1f50de665e4e3f3290d
Nathaniel Joshua <natha113322@gmail.com>
2021-06-23 05:56:29 -0700

    Write a program that adds two integer values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add two integer values and return the result
+ * @a: an integer arg
+ * @b: an integer arg
+ * Return: void.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

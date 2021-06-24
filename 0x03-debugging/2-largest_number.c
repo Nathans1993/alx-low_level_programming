6a266df70cadc8000666ca7112704d5573733b65
Nathaniel Joshua <natha113322@gmail.com>
2021-06-24 09:05:08 -0700

    Debugging conditional Statement

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b < c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}

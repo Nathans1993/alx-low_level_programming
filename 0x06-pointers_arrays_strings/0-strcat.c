17f506df1c51bf9683fed7632de51c0aaf01f6df
Nathaniel Joshua <natha113322@gmail.com>
2021-07-01 02:05:53 -0700

    Write a function that concatinate two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while(*(++dest));
-	while(*(dest++)= *(src++));
-	
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+		j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
 }

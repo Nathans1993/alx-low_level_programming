7ee641ce74f44dbb1866a0cf519118e3e255bef9
Nathaniel Joshua <natha113322@gmail.com>
2021-07-05 05:57:34 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "Holberton.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the momery to print
+ *
+ * Return: Nothing.
+ */
+void char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	for(i=0; i<n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}

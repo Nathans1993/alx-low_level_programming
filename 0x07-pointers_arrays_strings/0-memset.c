9f1e16cb78b195a4e7d7d2d0e6da63cb653b3fc6
Nathaniel Joshua <natha113322@gmail.com>
2021-07-05 06:21:17 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 
-
 /**
- * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
- * Return: the pointer to dest.
+ * *_memset - fills memory with a constant byte
+ * @n: # of bytes to be filled
+ * @b: value of the integer
+ * @s: memory area
+ * Return: s
  */
-
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while(n>0)
+
+	while (n > 0)
 	{
 		s[n - 1] = b;
 		n--;

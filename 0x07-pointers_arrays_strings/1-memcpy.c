e8923d7d354ba5f4a02611991e8a5c15336f7b03
Nathaniel Joshua <natha113322@gmail.com>
2021-07-05 07:12:11 -0700

    Function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
- * _memcpy - copies a block of data from a source address to a destination address.
- * @dest: This is a pointer to destination.
- * @src: This is a pointer to source of data to be copied.
- * @n: This is the number of bytes to be copied
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
 	return (dest);
 }

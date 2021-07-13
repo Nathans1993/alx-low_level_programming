b142615af91e9483d61fdccc426dd7aeb7668956
Nathaniel Joshua <natha113322@gmail.com>
2021-07-13 09:39:27 -0700

    Program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the number of arguments passes into it
+ * @argc: count of arguments
+ * @argv: array of pointers to the strings passed
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+        int a, b, Result;
+
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+        a = atoi(argv[1]);
+        b = atoi(argv[2]);
+        Result = a * b;
+        printf("%d\n", Result);
+
+        return (0);
+}
+~
+~

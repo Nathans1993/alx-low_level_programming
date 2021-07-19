af018aaa065a644a446a438b2cb0675355c07308
Nathaniel Joshua <natha113322@gmail.com>
2021-07-19 13:33:24 -0700

    header file

--- 0X0E-structures_typedef/dog.h
+++ 0X0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+
 /**
  * struct dog - define a new type structure dog
  * @name: Name of dog

4f34198f1caea95773f0f74689d2f2a57cd769f7
Nathaniel Joshua <natha113322@gmail.com>
2021-07-19 09:57:16 -0700

    Write a program that pints the name of the file it was compiled from followed by new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the name of the file it was complied from
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
